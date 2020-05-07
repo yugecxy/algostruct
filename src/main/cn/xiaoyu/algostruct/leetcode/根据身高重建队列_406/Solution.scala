33de2bb8f5fba5d1e45b89bec47a43bd05cb11bc
chenxiaoyu <2580246520@qq.com>
2020-05-07 08:26:20 +0800

    重建队列

---
+++ src/main/cn/xiaoyu/algostruct/leetcode/根据身高重建队列_406/Solution.scala
@@ -0,0 +1,34 @@
+package xiaoyu.algostruct.leetcode.根据身高重建队列_406
+
+import scala.collection.mutable
+
+object Solution {
+
+  def reconstructQueue(people: Array[Array[Int]]): Array[Array[Int]] = {
+    val sorted = people.sortBy(x => (-x(0), x(1)))
+    val res = mutable.ArrayBuffer[Array[Int]]()
+    for (i <- sorted.indices) {
+      val rank = sorted(i)(1)
+      if (rank >= res.length) res.append(sorted(i))
+      else res.insert(rank, sorted(i))
+    }
+    res.toArray
+  }
+
+  def main(args: Array[String]): Unit = {
+    reconstructQueue(
+      Array(
+        Array(9, 0),
+        Array(7, 0),
+        Array(1, 9),
+        Array(3, 0),
+        Array(2, 7),
+        Array(5, 3),
+        Array(6, 0),
+        Array(3, 4),
+        Array(6, 2),
+        Array(5, 2)
+      )
+    ).foreach(x => println(x.mkString(",")))
+  }
+}
