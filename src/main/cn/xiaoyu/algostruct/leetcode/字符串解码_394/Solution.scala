70e7daf7e5932ec104a426e70df4957381bbeb0b
chenxiaoyu <2580246520@qq.com>
2020-05-17 07:25:33 +0800

    字符串解码：栈实现

--- src/main/cn/xiaoyu/algostruct/leetcode/字符串解码_394/Solution.scala
+++ src/main/cn/xiaoyu/algostruct/leetcode/字符串解码_394/Solution.scala
@@ -1,24 +1,47 @@
 package xiaoyu.algostruct.leetcode.字符串解码_394
 
+import scala.collection.mutable.Stack
+
 object Solution {
 
+  /**
+    * 栈实现
+    */
   def decodeString(s: String): String = {
-    def compute(start: Int): (Int, String) = {
-      var index = start
-
-      var res = ""
-      while (index < s.length) {
-        val cur = s(index)
-        if (cur == 'n') res += compute(index)
-        else if (cur == ']') res += res * 3
-        else res += cur.toString
+    val stack = new Stack[(Int, String)]()
+    var multi = ""
+    var res = ""
+    for (c <- s) {
+      if ('0' <= c && c <= '9') {
+        multi += c
       }
-      (2, res)
+      else if (c == '[') {
+        stack.push((multi.toInt, res))
+        multi = ""
+        res = ""
+      }
+      else if (c == ']') {
+        val (last_multi, last_str) = stack.pop()
+        val curStr = res
+        for (i <- 0 until last_multi - 1) {
+          res += curStr
+        }
+        res = last_str + res
+      } else {
+        res += c
+      }
     }
+    res
+  }
+
+  /**
+    * 递归实现
+    */
+  def decodeString2(s: String): String = {
     ""
   }
 
   def main(args: Array[String]): Unit = {
-    println(decodeString(")()())"))
+    println(decodeString("3[a]2[b4[F]c]"))
   }
 }
