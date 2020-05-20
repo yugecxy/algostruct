37c5eedd62a7858fc5fc14931c7eecbc691ab4e8
chenxiaoyu <2580246520@qq.com>
2020-05-21 07:21:59 +0800

    最长上升子序列 tmp（未解决）

---
+++ src/main/cn/xiaoyu/algostruct/leetcode/最长上升子序列_300/Solution.scala
@@ -0,0 +1,21 @@
+package xiaoyu.algostruct.leetcode.最长上升子序列_300
+
+object Solution {
+
+  def lengthOfLIS(nums: Array[Int]): Int = {
+    var curMin = Int.MaxValue
+    var res = 1
+    for (i <- nums.indices) {
+      val cur = nums(i)
+      if (cur < curMin) {
+        res = math.max(res, nums.slice(i + 1, nums.length).count(_ >= cur))
+      }
+      curMin = math.min(curMin, cur)
+    }
+    res
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(lengthOfLIS(Array(10,9,2,17,9,7,101,18)))
+  }
+}
