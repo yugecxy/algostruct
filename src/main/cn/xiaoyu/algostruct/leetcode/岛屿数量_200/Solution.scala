e15f6296da81c1fdcbd8dcd12871201ea3f1065a
chenxiaoyu <2580246520@qq.com>
2020-05-25 08:59:09 +0800

    岛屿数量

---
+++ src/main/cn/xiaoyu/algostruct/leetcode/岛屿数量_200/Solution.scala
@@ -0,0 +1,40 @@
+package xiaoyu.algostruct.leetcode.岛屿数量_200
+
+import scala.collection.mutable
+
+object Solution {
+
+  def numIslands(grid: Array[Array[Char]]): Int = {
+    val map = mutable.HashMap[(Int, Int), String]()
+
+    def recursion(i: Int, j: Int): Unit = {
+      if (i >= grid.length || j >= grid(0).length || i < 0 || j < 0) return
+      if (grid(i)(j) == '1' && !map.contains((i, j))) {
+        map.put((i, j), null)
+        recursion(i + 1, j)
+        recursion(i, j + 1)
+        recursion(i - 1, j)
+        recursion(i, j - 1)
+      }
+    }
+
+    var res = 0
+    for (i <- grid.indices)
+      for (j <- grid(0).indices) {
+        if (!map.contains((i, j)) && grid(i)(j) == '1') {
+          res += 1
+          recursion(i, j)
+        }
+      }
+    res
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(numIslands(
+      Array(Array('1', '1', '1'),
+        Array('0', '1', '0'),
+        Array('1', '1', '1')
+      ))
+    )
+  }
+}
--- src/main/cn/xiaoyu/algostruct/test/Solution.scala
+++ src/main/cn/xiaoyu/algostruct/test/Solution.scala
@@ -3,19 +3,26 @@ package cn.xiaoyu.algostruct.test
 
 object Solution {
 
-  def main(args: Array[String]): Unit = {
-    for (i <- 1000 until 2000) //定义外层循环,求一千以内的质数,循环输出从2到999,1000可直接舍弃
-      if (i == 2 || i == 3) //如果当i=2和3,直接打印
-        println(i)
-      else if (ss(i)) //调用判断i是否是质数的方法,当i非2和3的时候开始判定,是的话就打印输出
-        println(i)
-
-    def ss(i: Int): Boolean = { //定义判断传入参数是否是质数的函数 返回布尔型值
-      for (j <- 2 until i) { //内层循环,使j的值从2循环到传入的i的值而非i,然后依次判断i/j是否有余数,
-        if ((i % j) == 0) //只要能被整除那就一定不是质数,所以结束函数
-          return false
+  def compute(obj: Any): Any = {
+    obj match {
+      case _: Map[String, Any] => {
+        obj.asInstanceOf[Map[String, Any]].map(x => (x._1, compute(x._2))).filter(x => x._2 match {
+          case _: Map[String, Any] => x._2.asInstanceOf[Map[String, Any]].nonEmpty
+          case _ => x._2 != ""
+        })
       }
-      true //循环结束都不能被整除的就一定是质数,返回true
+      case _: List[Any] => obj.asInstanceOf[List[Any]].map(x => compute(x)).filter(x => x match {
+        case _: Map[String, Any] => x.asInstanceOf[Map[String, Any]].nonEmpty
+        case _ => true
+      })
+      case _ => obj
     }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val a: Map[String, Any] = Map("name" -> "tom", "age" -> "", "info" -> List(Map("address" -> "hh", "kk" -> ""), Map("a" -> "")), "info2" -> Map("d" -> ""))
+    val b = compute(a)
+    println(b)
+
   }
 }
