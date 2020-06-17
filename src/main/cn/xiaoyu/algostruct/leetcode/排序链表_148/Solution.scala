f5753945d02d9f4b6f82fe2e7ea51fb5c35f3f69
chenxiaoyu <2580246520@qq.com>
2020-06-17 08:25:48 +0800

    排序链表

---
+++ src/main/cn/xiaoyu/algostruct/leetcode/排序链表_148/Solution.scala
@@ -0,0 +1,13 @@
+package xiaoyu.algostruct.leetcode.排序链表_148
+
+class ListNode(var _x: Int = 0) {
+  var next: ListNode = null
+  var x: Int = _x
+}
+
+object Solution {
+
+  def main(args: Array[String]): Unit = {
+    println()
+  }
+}
