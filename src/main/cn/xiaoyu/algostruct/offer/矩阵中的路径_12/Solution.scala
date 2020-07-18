af4a6b21314594a101adccaf7b57a02e183ba3f7
陈小雨 <chenxiaoyu@test.com>
2020-07-18 18:01:34 +0800

    矩阵中的路径

--- src/main/cn/xiaoyu/algostruct/offer/矩阵中的路径_12/Solution.scala
+++ src/main/cn/xiaoyu/algostruct/offer/矩阵中的路径_12/Solution.scala
@@ -1,7 +1,30 @@
 package xiaoyu.algostruct.offer.矩阵中的路径_12
 
+
 object Solution {
   def exist(board: Array[Array[Char]], word: String): Boolean = {
-    true
+    val m = board.length
+    val n = board.head.length
+
+    def dfs(x: Int, y: Int, k: Int): Boolean = {
+      if (k > word.length - 1) return true
+      if (x < 0 || y < 0 || x > m - 1 || y > n - 1 || board(x)(y) != word(k)) return false
+      val tmp = board(x)(y)
+      board(x)(y) = '/'
+      val res = dfs(x - 1, y, k + 1) || dfs(x + 1, y, k + 1) || dfs(x, y - 1, k + 1) || dfs(x, y + 1, k + 1)
+      board(x)(y) = tmp
+      res
+    }
+
+    if (board.isEmpty) return false
+    else {
+      board.indices.foreach(x => {
+        board.head.indices.foreach(y => {
+          val res = dfs(x, y, 0)
+          if (res) return true
+        })
+      })
+    }
+    false
   }
 }
