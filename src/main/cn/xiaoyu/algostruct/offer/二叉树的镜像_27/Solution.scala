81810b3d107d7fcec537d6830ce6444708975b84
chenxiaoyu <2580246520@qq.com>
2020-08-14 07:58:39 +0800

    二叉树的镜像

---
+++ src/main/cn/xiaoyu/algostruct/offer/二叉树的镜像_27/Solution.scala
@@ -0,0 +1,28 @@
+package xiaoyu.algostruct.offer.二叉树的镜像_27
+
+class TreeNode(var _value: Int) {
+  var value: Int = _value
+  var left: TreeNode = null
+  var right: TreeNode = null
+}
+
+object Solution {
+  def mirrorTree(root: TreeNode): TreeNode = {
+    if (root == null) return null
+    val tmpNode = new TreeNode(root.value)
+    tmpNode.right = mirrorTree(root.left)
+    tmpNode.left = mirrorTree(root.right)
+    tmpNode
+  }
+
+  def main(args: Array[String]): Unit = {
+    val a1 = new TreeNode(1)
+    val a2 = new TreeNode(2)
+    val a3 = new TreeNode(3)
+    a1.left = a2
+    a1.right = a3
+
+    val a = mirrorTree(a1)
+    println(1111)
+  }
+}
