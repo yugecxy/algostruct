4feb0eb2cafefb87c5449df86314d22b57a34ee8
chenxiaoyu <2580246520@qq.com>
2020-08-17 08:36:39 +0800

    二叉树的最近公共祖先

---
+++ src/main/cn/xiaoyu/algostruct/offer/二叉树的最近公共祖先_68/Solution.scala
@@ -0,0 +1,21 @@
+package xiaoyu.algostruct.offer.二叉树的最近公共祖先_68
+
+class TreeNode(var _value: Int) {
+  var value: Int = _value
+  var left: TreeNode = null
+  var right: TreeNode = null
+}
+
+/**
+  * 后续遍历
+  */
+class Solution {
+  def lowestCommonAncestor(root: TreeNode, p: TreeNode, q: TreeNode): TreeNode = {
+    if (root == null || root == p || root == q) return root
+    val left = lowestCommonAncestor(root.left, p, q)
+    val right = lowestCommonAncestor(root.right, p, q)
+    if (left == null) return right
+    if (right == null) return left
+    root
+  }
+}
