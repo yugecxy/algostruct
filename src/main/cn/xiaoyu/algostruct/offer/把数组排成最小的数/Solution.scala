8bd09b0454b2333033ccc2e193e0f937827d1d9e
chenxiaoyu <2580246520@qq.com>
2020-11-01 11:47:55 +0800

    2020 11 1

---
+++ src/main/cn/xiaoyu/algostruct/offer/把数组排成最小的数/Solution.scala
@@ -0,0 +1,14 @@
+package xiaoyu.algostruct.offer.把数组排成最小的数
+
+/**
+  * 自定义排序规则，然后套用排序的思想即可
+  */
+object Solution {
+  def minNumber(nums: Array[Int]): String = {
+    ""
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(minNumber(Array(10, 2)))
+  }
+}
---
+++ src/main/cn/xiaoyu/algostruct/offer/股票的最大利润/Solution.scala
@@ -0,0 +1,17 @@
+package xiaoyu.algostruct.offer.股票的最大利润
+
+object Solution {
+  def maxProfit(prices: Array[Int]): Int = {
+    var min = Integer.MAX_VALUE
+    var maxProfit = 0
+    prices.foreach(p => {
+      maxProfit = Math.max(maxProfit, p - min)
+      min = Math.min(p, min)
+    })
+    maxProfit
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(maxProfit(Array(7,1,5,3,6,4)))
+  }
+}
