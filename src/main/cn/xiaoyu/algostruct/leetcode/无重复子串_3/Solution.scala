565b70b5242661a033cbee913a887de3942328a7
chenxiaoyu <2580246520@qq.com>
2020-12-09 22:01:48 +0800

    无重复子串

--- src/main/cn/xiaoyu/algostruct/leetcode/无重复子串_3/Solution.scala
+++ src/main/cn/xiaoyu/algostruct/leetcode/无重复子串_3/Solution.scala
@@ -1,21 +1,25 @@
-package xiaoyu.algostruct.leetcode.无重复子串_3
+package cn.xiaoyu.algostruct.leetcode.无重复子串_3
 
 import scala.collection.mutable
 
 object Solution {
   def lengthOfLongestSubstring(s: String): Int = {
     var max = 0
-    var len = 0
-    val map = new mutable.HashMap[Char, Int]()
-    for (i <- s.indices) {
-      len += 1
-      val curChar = s(i)
-      if (map.contains(curChar) && i - map(curChar) < len) {
-        len = i - map(curChar)
+    var begin = 0
+    val map = mutable.HashMap[Char, Int]()
+    s.zipWithIndex.foreach { case (c, index) => {
+      if (!map.contains(c)) map.put(c, index)
+      else {
+        begin = math.max(map(c) + 1, begin)
+        map.put(c, index)
       }
-      map.put(curChar, i)
-      max = math.max(len, max)
+      max = math.max(index - begin + 1, max)
+    }
     }
     max
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(lengthOfLongestSubstring("tmmzuxt"))
   }
 }
--- src/main/cn/xiaoyu/algostruct/leetcode/组合总和_39/Solution.scala
+++
@@ -1,7 +0,0 @@
-package cn.xiaoyu.algostruct.leetcode.组合总和_39
-
-object Solution {
-  def combinationSum(candidates: Array[Int], target: Int): List[List[Int]] = {
-    null
-  }
-}
