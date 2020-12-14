4937fdcdf3bf031173a0fa32e0a0278a4ca4b2f1
陈小雨 <chenxiaoyu@test.com>
2020-12-14 15:16:53 +0800

    顺时针打印矩阵

--- src/main/cn/xiaoyu/algostruct/offer/顺时针打印矩阵/Solution.scala
+++ src/main/cn/xiaoyu/algostruct/offer/顺时针打印矩阵/Solution.scala
@@ -1,46 +1,57 @@
-package xiaoyu.algostruct.offer.顺时针打印矩阵
+package cn.xiaoyu.algostruct.offer.顺时针打印矩阵
 
+import scala.collection.mutable
+
+/**
+  * https://leetcode-cn.com/problems/shun-shi-zhen-da-yin-ju-zhen-lcof/
+  */
 object Solution {
   def spiralOrder(matrix: Array[Array[Int]]): Array[Int] = {
     if (matrix.isEmpty) return Array[Int]()
 
     val length = matrix.head.length
     val width = matrix.length
-    var res: Array[Int] = Array()
+    val res = mutable.ArrayBuffer[Int]()
+    val records = mutable.HashSet[(Int, Int)]()
 
     /**
       * @param dire 0:右 1:下 2:左 3:上
       */
     def recursion(i: Int, j: Int, dire: Int): Unit = {
-      val cur = matrix(i)(j)
-      res :+= cur
-      matrix(i)(j) = Int.MinValue
+      if (i < 0 || i > width - 1 || j < 0 || j > length - 1 || records.contains((i, j))) return
+
+      res.append(matrix(i)(j))
+      records.add((i, j))
 
       dire match {
         case 0 => {
-          if (j < length - 1 && matrix(i)(j + 1) != Int.MinValue) recursion(i, j + 1, 0)
-          else if (i < width - 1 && matrix(i + 1)(j) != Int.MinValue) recursion(i + 1, j, 1)
+          if (j < length - 1 && !records.contains((i, j + 1))) recursion(i, j + 1, 0)
+          else recursion(i + 1, j, 1)
         }
         case 1 => {
-          if (i < width - 1 && matrix(i + 1)(j) != Int.MinValue) recursion(i + 1, j, 1)
-          else if (j > 0 && matrix(i)(j - 1) != Int.MinValue) recursion(i, j - 1, 2)
+          if (i < width - 1 && !records.contains((i + 1, j))) recursion(i + 1, j, 1)
+          else recursion(i, j - 1, 2)
         }
         case 2 => {
-          if (j > 0 && matrix(i)(j - 1) != Int.MinValue) recursion(i, j - 1, 2)
-          else if (i > 0 && matrix(i - 1)(j) != Int.MinValue) recursion(i - 1, j, 3)
+          if (j > 0 && !records.contains((i, j - 1))) recursion(i, j - 1, 2)
+          else recursion(i - 1, j, 3)
         }
         case 3 => {
-          if (i > 0 && matrix(i - 1)(j) != Int.MinValue) recursion(i - 1, j, 3)
-          else if (j < length - 1 && matrix(i)(j + 1) != Int.MinValue) recursion(i, j + 1, 0)
+          if (i > 0 && !records.contains((i - 1, j))) recursion(i - 1, j, 3)
+          else recursion(i, j + 1, 0)
         }
       }
     }
 
     recursion(0, 0, 0)
-    res
+    res.toArray
   }
 
   def main(args: Array[String]): Unit = {
-
+    println(spiralOrder(Array(
+      Array(1, 2, 3),
+      Array(4, 5, 6),
+      Array(7, 8, 9)
+    )).toSeq)
   }
 }
