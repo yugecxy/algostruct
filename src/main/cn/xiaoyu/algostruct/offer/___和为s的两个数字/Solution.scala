e1f94d1e8bbd7de98771272b6cd46927990642dc
chenxiaoyu <2580246520@qq.com>
2020-12-30 09:04:39 +0800

    和为s的两个数字

---
+++ src/main/cn/xiaoyu/algostruct/offer/___和为s的两个数字/Solution.scala
@@ -0,0 +1,14 @@
+package cn.xiaoyu.algostruct.offer.___和为s的两个数字
+
+/**
+  * https://leetcode-cn.com/problems/he-wei-sde-liang-ge-shu-zi-lcof/
+  */
+object Solution {
+
+  /**
+    * 时间复杂度O(n)，空间复杂度O(1)
+    */
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+    Array()
+  }
+}
