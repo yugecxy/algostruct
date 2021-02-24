779e5c417796fcd60dd5f530829d886c75beb699
chenxiaoyu <2580246520@qq.com>
2021-02-25 07:56:11 +0800

    最大子序和

--- src/main/cn/xiaoyu/algostruct/test/Solution.scala
+++ src/main/cn/xiaoyu/algostruct/test/Solution.scala
@@ -1,12 +1,22 @@
-package xiaoyu.algostruct.test
-
-
-object Solution {
-  def decodeString(s: String): String = {
-    ""
-  }
-
-  def main(args: Array[String]): Unit = {
-
-  }
-}
+//package cn.xiaoyu.algostruct.test
+//
+//class CachedNode(key: Int) {
+//  var value: Int = key
+//  var next: CachedNode = null
+//  var pre: CachedNode = null
+//}
+//
+//object Solution {
+//  def main(args: Array[String]): Unit = {
+//    val a = new CachedNode(2)
+//
+//    def x(node: CachedNode) = {
+//      node.value = 1
+//    }
+//
+//    x(a)
+//
+//    println(a.value)
+//
+//  }
+//}
