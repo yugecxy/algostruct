70980f0525c6a29252e5f141c439a16c4613e091
chenxiaoyu <2580246520@qq.com>
2021-03-02 18:44:56 +0800

    最大利润

--- src/main/cn/xiaoyu/algostruct/offer/股票的最大利润/Solution.scala
+++ src/main/cn/xiaoyu/algostruct/offer/股票的最大利润/Solution.scala
@@ -14,7 +14,28 @@ object Solution {
     maxProfit
   }
 
+
+  //求最大利润并且同时返回买卖的点
+  def maxProfit2(prices: Array[Int]): (Int, Int, Int) = {
+    var min = Int.MaxValue
+    var minIndex = -1
+    var maxIndex = -1
+    var max = Int.MinValue
+    prices.indices.foreach(index => {
+      val curPrice = prices(index)
+      if (curPrice < min) {
+        min = curPrice
+        minIndex = index
+      }
+      if (curPrice - min > max) {
+        max = curPrice - min
+        maxIndex = index
+      }
+    })
+    (max, minIndex, maxIndex)
+  }
+
   def main(args: Array[String]): Unit = {
-    println(maxProfit(Array(7, 1, 5, 3, 6, 4)))
+    println(maxProfit2(Array(7, 1, 5, 3, 6, 4)))
   }
 }
