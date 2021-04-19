543d23aa21255373f1228e1ed147ef2206df66c7
chenxiaoyu <2580246520@qq.com>
2021-04-19 09:27:32 +0800

    add top

---
+++ src/main/cn/xiaoyu/algostruct/leetcode/寻找重复数/Solution.scala
@@ -0,0 +1,28 @@
+package cn.xiaoyu.algostruct.leetcode.寻找重复数
+
+object Solution {
+  // 方法一：采用位图。时间复杂度O(n)，空间复杂度 O(n)
+  def findDuplicate(nums: Array[Int]): Int = {
+    val bitMap = new Array[Byte](nums.length)
+    nums.foreach(num => {
+      if (bitMap(num) == '1') return num
+      bitMap(num) = '1'
+    })
+    -1
+  }
+
+  // 方法二: 采用快速选择算法。时间复杂度O(nlogn), 空间复杂度 O(1)
+  def findDuplicate2(nums: Array[Int]): Int = {
+    -1
+  }
+
+  // 方法三: 采用快慢指针。时间复杂度O(n), 空间复杂度 O(1)
+  def findDuplicate3(nums: Array[Int]): Int = {
+    -1
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    println(findDuplicate(Array(1, 1, 2, 3, 4)))
+  }
+}
--- src/main/cn/xiaoyu/algostruct/test/Solution.scala
+++ src/main/cn/xiaoyu/algostruct/test/Solution.scala
@@ -1,22 +1,33 @@
-//package cn.xiaoyu.algostruct.test
-//
-//class CachedNode(key: Int) {
-//  var value: Int = key
-//  var next: CachedNode = null
-//  var pre: CachedNode = null
-//}
-//
-//object Solution {
-//  def main(args: Array[String]): Unit = {
-//    val a = new CachedNode(2)
-//
-//    def x(node: CachedNode) = {
-//      node.value = 1
-//    }
-//
-//    x(a)
-//
-//    println(a.value)
-//
-//  }
-//}
+package cn.xiaoyu.algostruct.test
+
+object Solution {
+
+  def merge(arr1: Array[Int], arr2: Array[Int]): Array[Int] = {
+    var i = 0
+    var j = 0
+    var cnt = 0
+    val newArr = new Array[Int](arr1.length + arr2.length)
+    while (cnt < arr1.length + arr2.length) {
+      if (i >= arr1.length || (j < arr2.length && arr1(i) > arr2(j))) {
+        newArr(cnt) = arr2(j)
+        j += 1
+      } else {
+        newArr(cnt) = arr1(i)
+        i += 1
+      }
+      cnt += 1
+    }
+    newArr
+  }
+
+  def mergeSort(arr: Array[Int]): Array[Int] = {
+    if (arr.length == 1) return arr
+    val midIndex = (arr.length - 1) / 2
+
+    merge(mergeSort(arr.slice(0, midIndex + 1)), mergeSort(arr.slice(midIndex + 1, arr.length)))
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(mergeSort(Array(2, 1)).toSeq)
+  }
+}
