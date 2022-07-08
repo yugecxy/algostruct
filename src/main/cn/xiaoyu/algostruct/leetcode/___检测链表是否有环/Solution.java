02b746065286354f65c3509a8eee04d7c1328917
chenxiaoyu <2580246520@qq.com>
2022-07-08 14:41:27 +0800

    循环链表

--- src/main/cn/xiaoyu/algostruct/leetcode/___检测链表是否有环/Solution.java
+++ src/main/cn/xiaoyu/algostruct/leetcode/___检测链表是否有环/Solution.java
@@ -1,8 +1,21 @@
 package xiaoyu.algostruct.leetcode.___检测链表是否有环;
 
+import xiaoyu.algostruct.helper.Node;
+
 /**
  * <a href="https://leetcode-cn.com/problems/linked-list-cycle/">...</a>
  */
 public class Solution {
-
+    public boolean hasCycle(Node head) {
+        Node fast = head;
+        Node slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
