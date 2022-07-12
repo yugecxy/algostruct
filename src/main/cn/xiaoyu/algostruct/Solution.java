14184824ee62303c0e16a7612efb85fa95e9fd04
chenxiaoyu <2580246520@qq.com>
2022-07-12 16:35:05 +0800

    有效括号

--- src/main/cn/xiaoyu/algostruct/Solution.java
+++ src/main/cn/xiaoyu/algostruct/Solution.java
@@ -1,12 +1,8 @@
 package xiaoyu.algostruct;
 
-class Test {
-
-}
-
 public class Solution {
-    public static void main(String[] args) {
-        int i = '3' - '0';
-        System.out.println(i);
+    public boolean isValid(String s) {
+
+        return true;
     }
 }
---
+++ src/main/cn/xiaoyu/algostruct/归类/栈/有效的括号/Solution.java
@@ -0,0 +1,31 @@
+package xiaoyu.algostruct.归类.栈.有效的括号;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Map<Character, Character> map = new HashMap<Character, Character>() {{
+            put(')', '(');
+            put(']', '[');
+            put('}', '{');
+        }};
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                if (stack.isEmpty() || stack.pop() != map.get(c)) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
