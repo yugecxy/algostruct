1ef8ecef7239a305de3ae0a2ce451c3bfa6acf9d
chenxiaoyu <2580246520@qq.com>
2022-07-14 15:40:55 +0800

    双指针删除倒数第k大

--- src/main/cn/xiaoyu/algostruct/归类/链表/删除链表倒数第n个节点/Solution.java
+++ src/main/cn/xiaoyu/algostruct/归类/链表/删除链表倒数第n个节点/Solution.java
@@ -1,7 +1,34 @@
 package xiaoyu.algostruct.归类.链表.删除链表倒数第n个节点;
 
+
+import xiaoyu.algostruct.helper.ListNode;
+
 /**
  * <a href="https://leetcode.cn/problems/remove-nth-node-from-end-of-list/">...</a>
  */
 public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode prev = null;
+        int diff = 0;
+        while (fast != null) {
+            if (diff >= n) {
+                prev = slow;
+                slow = slow.next;
+            }
+            fast = fast.next;
+            diff += 1;
+        }
+
+        //删除
+        if (prev == null) {
+            return head.next;
+        }
+        prev.next = slow == null ? null : slow.next;
+        return head;
+    }
 }
