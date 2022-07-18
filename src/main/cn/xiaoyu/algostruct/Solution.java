8c24ca07a2cd618fca838596a157bbd83f485839
chenxiaoyu <2580246520@qq.com>
2022-07-18 17:33:29 +0800

    最长回文

--- src/main/cn/xiaoyu/algostruct/Solution.java
+++ src/main/cn/xiaoyu/algostruct/Solution.java
@@ -1,20 +1,28 @@
 package xiaoyu.algostruct;
 
-import java.util.Queue;
-import java.util.concurrent.LinkedBlockingQueue;
-
 public class Solution {
-    public int missingNumber(int[] nums) {
-        int i = 0, j = nums.length - 1;
-        while (i <= j) {
-            int m = (i + j) / 2;
-            if (nums[m] == m) i = m + 1;
-            else j = m - 1;
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
         }
-        return i;
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
     }
 
-    public static void main(String[] args) {
-
+    public int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            --left;
+            ++right;
+        }
+        return right - left - 1;
     }
 }
