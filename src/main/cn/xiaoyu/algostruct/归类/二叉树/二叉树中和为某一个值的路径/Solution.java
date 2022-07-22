b2f5ff98b87ffaf292b511f19291ab79d620ae50
chenxiaoyu <2580246520@qq.com>
2022-07-22 10:31:46 +0800

    pathSum

--- src/main/cn/xiaoyu/algostruct/归类/二叉树/二叉树中和为某一个值的路径/Solution.java
+++ src/main/cn/xiaoyu/algostruct/归类/二叉树/二叉树中和为某一个值的路径/Solution.java
@@ -1,10 +1,14 @@
-package xiaoyu.algostruct;
+package xiaoyu.algostruct.归类.二叉树.二叉树中和为某一个值的路径;
 
 import java.util.LinkedList;
 import java.util.List;
 
 import xiaoyu.algostruct.helper.TreeNode;
 
+/**
+ * <a href="https://leetcode.cn/problems/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof/">...</a>
+ * 二叉树中和为某一值的路径: 给你二叉树的根节点 root 和一个整数目标和 targetSum ，找出所有从根节点到叶子节点路径总和等于给定目标和的路径
+ */
 public class Solution {
     private List<List<Integer>> ret = new LinkedList<>();
 
