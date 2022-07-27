eaafab120d083757ce3cf03df287f0c8a569b6e7
chenxiaoyu <2580246520@qq.com>
2022-07-27 11:19:13 +0800

    最大路径和

---
+++ src/main/cn/xiaoyu/algostruct/归类/二叉树/二叉树中的最大路径和/Solution.java
@@ -0,0 +1,29 @@
+package xiaoyu.algostruct.归类.二叉树.二叉树中的最大路径和;
+
+import xiaoyu.algostruct.helper.TreeNode;
+
+/**
+ * <a href="https://leetcode-cn.com/problems/binary-tree-maximum-path-sum/">...</a>
+ */
+public class Solution {
+
+    private int maxValue = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return maxValue;
+    }
+
+    public int dfs(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = Math.max(0, dfs(root.left));
+        int right = Math.max(0, dfs(root.right));
+
+        maxValue = Math.max(maxValue, left + right + root.val);
+        int maxVal = Math.max(left, right);
+
+        return root.val + maxVal;
+    }
+
+}
