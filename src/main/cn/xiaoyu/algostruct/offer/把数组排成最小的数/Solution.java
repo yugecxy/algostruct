04b64a849883313215ab5a83c5b9f3e14500ef50
chenxiaoyu <2580246520@qq.com>
2022-08-05 11:24:42 +0800

    数组排成最小的

--- src/main/cn/xiaoyu/algostruct/offer/把数组排成最小的数/Solution.java
+++ src/main/cn/xiaoyu/algostruct/offer/把数组排成最小的数/Solution.java
@@ -1,7 +1,10 @@
-package xiaoyu.algostruct;
+package xiaoyu.algostruct.offer.把数组排成最小的数;
 
 import java.util.Arrays;
 
+/**
+ * <a href="https://leetcode-cn.com/problems/ba-shu-zu-pai-cheng-zui-xiao-de-shu-lcof/">...</a>
+ */
 public class Solution {
     public String minNumber(int[] nums) {
         String[] strings = new String[nums.length];
@@ -15,6 +18,6 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution().minNumber(new int[]{3, 30, 34, 5, 9}));
+        System.out.println(new xiaoyu.algostruct.Solution().minNumber(new int[]{3, 30, 34, 5, 9}));
     }
 }
