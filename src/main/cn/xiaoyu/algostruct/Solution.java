c4a2667658c973e9f4e54a7ef869685e86c3e965
chenxiaoyu <2580246520@qq.com>
2022-08-05 10:27:38 +0800

    机器人的运动范围

--- src/main/cn/xiaoyu/algostruct/Solution.java
+++ src/main/cn/xiaoyu/algostruct/Solution.java
@@ -1,24 +1,15 @@
 package xiaoyu.algostruct;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 import xiaoyu.algostruct.helper.TreeNode;
 
 public class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || root == p || root == q) {
-            return root;
-        }
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if (left == null) {
-            return right;
-        }
-        if (right == null) {
-            return left;
-        }
-        return root;
-    }
+
 
     public static void main(String[] args) {
-
+        System.out.println();
     }
 }
