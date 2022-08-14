6d492da82c84547305637e801b8c4b10c0be8686
chenxiaoyu <2580246520@qq.com>
2022-08-14 17:06:08 +0800

    全组合

--- src/main/cn/xiaoyu/algostruct/归类/组合排列/全组合/Solution.java
+++ src/main/cn/xiaoyu/algostruct/归类/组合排列/全组合/Solution.java
@@ -1,5 +1,31 @@
 package xiaoyu.algostruct.归类.组合排列.全组合;
 
-public class Solution {
+import java.util.LinkedList;
+import java.util.List;
 
+public class Solution {
+    public List<List<Integer>> ret = new LinkedList<>();
+
+    public List<List<Integer>> getAllSubSet(int[] nums) {
+        dfs(nums, new LinkedList<>(), 0);
+        return ret;
+    }
+
+    public void dfs(int nums[], List<Integer> trace, int step) {
+        if (step >= nums.length) {
+            ret.add(new LinkedList<>(trace));
+            return;
+        }
+        dfs(nums, trace, step + 1);
+
+        trace.add(nums[step]);
+        dfs(nums, trace, step + 1);
+        trace.remove(trace.size() - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3};
+        List<List<Integer>> ret = new Solution().getAllSubSet(nums);
+        System.out.println(111);
+    }
 }
