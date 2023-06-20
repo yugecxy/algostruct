c7578de231cfe9370af1188a1ffe078c4cf68d28
chenxiaoyu <xiaoyu.chen8@nio.com>
2023-06-20 16:43:55 +0800

    feat：机器人

---
+++ src/main/cn/xiaoyu/algostruct/basic/随记/Solution.java
@@ -0,0 +1,10 @@
+package xiaoyu.algostruct.basic.随记;
+
+public class Solution {
+    /**
+     * 位数之和：给一个整数，求各位之和。如：123->6
+     */
+    public int getSumBySplitBit(int num) {
+        return -1;
+    }
+}
---
+++ src/main/cn/xiaoyu/algostruct/tag/dp/MovingCount.java
@@ -0,0 +1,46 @@
+package xiaoyu.algostruct.tag.dp;
+
+/**
+ * 地上有一个m行n列的方格，从坐标 [0,0] 到坐标 [m-1,n-1] 。
+ * 一个机器人从坐标 [0, 0] 的格子开始移动，它每次可以向左、右、上、下移动一格（不能移动到方格外），
+ * 也不能进入行坐标和列坐标的数位之和大于k的格子。例如，当k为18时，机器人能够进入方格 [35, 37] ，
+ * 因为3+5+3+7=18。但它不能进入方格 [35, 38]，因为3+5+3+8=19。请问该机器人能够到达多少个格子？
+ * <a href="https://leetcode.cn/problems/ji-qi-ren-de-yun-dong-fan-wei-lcof/">...</a>
+ */
+public class MovingCount {
+    public int movingCount(int m, int n, int k) {
+        boolean[][] arr = new boolean[m][n];
+        int cnt = 0;
+        for (int x = 0; x < m; x++) {
+            for (int y = 0; y < n; y++) {
+                if (x == 0 && y == 0 && getSumBySplit(x) + getSumBySplit(y) <= k) {
+                    cnt += 1;
+                    arr[x][y] = true;
+                    continue;
+                }
+                boolean left = x - 1 >= 0 && arr[x - 1][y];
+                boolean right = x + 1 < m && arr[x + 1][y];
+                boolean up = y + 1 < n && arr[x][y + 1];
+                boolean down = y - 1 >= 0 && arr[x][y - 1];
+                boolean cur = (left || right || up || down) && getSumBySplit(x) + getSumBySplit(y) <= k;
+                if (cur) {
+                    cnt += 1;
+                    arr[x][y] = true;
+                }
+            }
+        }
+        return cnt;
+    }
+
+    public int getSumBySplit(int num) {
+        int sum = 0;
+        for (Character i : String.valueOf(num).toCharArray()) {
+            sum += Integer.parseInt(i.toString());
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new MovingCount().movingCount(3, 1, 0));
+    }
+}
