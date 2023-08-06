d40b25ca0352277ff310b6529e9904cfae17a0cd
xiaoyu.chen8 <xiaoyu.chen8@nio.com>
2023-08-06 16:38:46 +0800

    feat: 观察者

---
+++ src/main/cn/xiaoyu/algostruct/design/observer/Observer.java
@@ -0,0 +1,35 @@
+package xiaoyu.algostruct.design.observer;
+
+interface baseObserver {
+  void deal();
+}
+
+class Observer1 implements baseObserver {
+  @Override
+  public void deal() {
+
+  }
+}
+
+class Observer2 implements baseObserver {
+  @Override
+  public void deal() {
+
+  }
+}
+
+class Subscribe {
+  public void register() {
+
+  }
+
+  public void remove() {
+
+  }
+
+}
+
+public class Observer {
+  //注册观察者
+  //遍历观察者
+}
