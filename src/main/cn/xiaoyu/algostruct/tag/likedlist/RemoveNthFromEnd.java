55853113228414f3aa603120886964da26139b27
xiaoyu.chen8 <xiaoyu.chen8@nio.com>
2023-11-17 17:28:28 +0800

    feat: 删除倒数第n个节点

--- src/main/cn/xiaoyu/algostruct/tag/likedlist/RemoveNthFromEnd.java
+++ src/main/cn/xiaoyu/algostruct/tag/likedlist/RemoveNthFromEnd.java
@@ -7,6 +7,43 @@ import xiaoyu.algostruct.helper.ListNode;
  */
 public class RemoveNthFromEnd {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        return null;
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode prev = null;
+        int cnt = 1;
+        while (fast.next != null) {
+            if (cnt >= n) {
+                prev = slow;
+                slow = slow.next;
+            }
+            fast = fast.next;
+            cnt += 1;
+        }
+        if (prev == null) {
+            head = head.next;
+        } else {
+            prev.next = slow.next;
+        }
+        return head;
+    }
+
+    public ListNode removeNthFromStart(ListNode head, int n) {
+        ListNode prev = null;
+        ListNode cur = head;
+        int cnt = 0;
+        while (cur != null) {
+            cnt += 1;
+            if (cnt == n) {
+                if (prev == null) {
+                    head = head.next;
+                } else {
+                    prev.next = cur.next;
+                }
+                break;
+            }
+            prev = cur;
+            cur = cur.next;
+        }
+        return head;
     }
 }
