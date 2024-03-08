0b5e64cf92f7af36b0184f6b7ac64754705b52ba
xiaoyu.chen8 <xiaoyu.chen8@nio.com>
2024-03-08 14:48:39 +0800

    feat: 小记

--- src/main/cn/xiaoyu/algostruct/basic/小记/Solution.java
+++ src/main/cn/xiaoyu/algostruct/basic/小记/Solution.java
@@ -5,7 +5,13 @@ public class Solution {
      * 位数之和：给一个整数，求各位之和。如：123->6
      */
     public int getSumBySplitBit(int num) {
-        return -1;
+        int a = num;
+        int sum = 0;
+        while (a != 0) {
+            sum += a % 10;
+            a = a / 10;
+        }
+        return sum;
     }
 
     /**
@@ -14,5 +20,28 @@ public class Solution {
     public void intToChar() {
         int a = '3' - '0'; //a = 3
         char ac = 3 + '0'; // ac = '3'
+
+        int b = '3'; //ascii值
+        char bc = 3; //ascii值为3的char
+
+        int c = 4;
+        char cc = (char) c; //使用变量需要强转
+    }
+
+    /**
+     * 位运算
+     */
+    public void t() {
+        //除以2/乘2
+        int a = 4 >> 1;
+        int b = 4 << 1;
+
+        //取余
+        int hash = 123145;
+        int bin = hash & 10;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().getSumBySplitBit(13));
     }
 }
